6ed3adbe0216135484f2ab4bdb743f27a592cb35
Himeka Tsuchida <yuria@sfc.wide.ad.jp>
2021-07-09 22:36:33 +0900

    [WIP]日付のフォーマットを整える手前まで終了。今日の日記も書いた。

--- components/layout.js
+++ components/layout.js
@@ -39,7 +39,7 @@ export default function Layout({ children, home }) {
                         <Link href="/">
                             <a>
                                 <img
-                                    src="/images/profile.jpg"
+                                    src="/images/profile_icon.png"
                                     className={`${styles.headerImage} ${utilStyles.borderCircle}`}
                                     alt={name}
                                 />
