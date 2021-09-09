fa104c855042436a6f0158177e4421a5b1d48cf5
Himeka Tsuchida <yuria@sfc.wide.ad.jp>
2021-09-09 20:55:08 +0900

    [WIP]ブログ記事投稿

--- pages/index.js
+++ pages/index.js
@@ -22,12 +22,8 @@ export default function Home({ allPostsData }) {
             </Head>
             <section className={utilStyles.headingMd}>
                 <p>慶應義塾大学環境情報学部４年/なんでも作れるWEBエンジニア志望</p>
-                <p>
-                    (This is a sample website - you’ll be building a site like this on{' '}
-                    <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-                </p>
+
             </section>
-            <section className={utilStyles.headingMd}>...</section>
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
                 <ul className={utilStyles.list}>
